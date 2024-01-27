ccbb8640709877f5dce8e027f5adae24495ce26c
Anish Das <98608261+Anish-Das-GH@users.noreply.github.com>
2024-01-27 11:00:37 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Kth smallest element/kth-smallest-element.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+		int t=sc.nextInt();
+		
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+		    
+		    int arr[]=new int[n];
+		    
+		    for(int i=0;i<n;i++)
+		    arr[i]=sc.nextInt();
+		    
+		    int k=sc.nextInt();
+		    Solution ob = new Solution();
+		    out.println(ob.kthSmallest(arr, 0, n-1, k));
+		}
+		out.flush();
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public static int kthSmallest(int[] arr, int l, int r, int k) 
+    { 
+        //Your code here
+        int i;
+        int size = (l+r+1);
+        PriorityQueue<Integer> heap = new PriorityQueue<Integer>();
+        for(i=0;i<size;i++)
+        {
+            heap.add(arr[i]);
+        }
+        for(i=1;i<k;i++)
+        {
+            heap.poll();
+        }
+        return heap.peek();
+        
+    } 
+}
