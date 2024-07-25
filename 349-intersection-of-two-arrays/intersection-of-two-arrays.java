3d326c8ecdeef8db2e9857709b26033da71eaef9
Anish Das <98608261+Anish-Das-GH@users.noreply.github.com>
2024-07-26 01:33:36 +0530

    Time: 2 ms (95.81%) | Memory: 43.6 MB (7.60%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,60 +1,60 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         //HashSet Method
-        // HashSet<Integer> set1 = new HashSet<>();
-        // HashSet<Integer> set2 = new HashSet<>();
-        // for(int i=0;i<nums1.length;i++)
-        // {
-        //     set1.add(nums1[i]);
-        // }
-        // for(int i=0;i<nums2.length;i++){
-        //     if(set1.contains(nums2[i]))
-        //     {
-        //         set2.add(nums2[i]);
-        //     }
-        // }
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+        for(int i=0;i<nums1.length;i++)
+        {
+            set1.add(nums1[i]);
+        }
+        for(int i=0;i<nums2.length;i++){
+            if(set1.contains(nums2[i]))
+            {
+                set2.add(nums2[i]);
+            }
+        }
 
-        // int[] arr = new int[set2.size()];
-        // int i=0;
-        // for(int x:set2)
-        // {
-        //     arr[i]=x;
-        //     i++;
-        // }
+        int[] arr = new int[set2.size()];
+        int i=0;
+        for(int x:set2)
+        {
+            arr[i]=x;
+            i++;
+        }
 
-        // return arr;
+        return arr;
 
         //Two Pointer 
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        HashSet<Integer> set = new HashSet<>();
-        int i=0;int j=0;
-        while(i<nums1.length && j<nums2.length)
-        {
-            if(nums1[i]==nums2[j])
-            {
-                set.add(nums1[i]);
-                i++;
-                j++;
-            }
-            else if(nums1[i]<nums2[j])
-            {
-                i++;
-            }
-            else
-            {
-                j++;
-            }
+        // Arrays.sort(nums1);
+        // Arrays.sort(nums2);
+        // HashSet<Integer> set = new HashSet<>();
+        // int i=0;int j=0;
+        // while(i<nums1.length && j<nums2.length)
+        // {
+        //     if(nums1[i]==nums2[j])
+        //     {
+        //         set.add(nums1[i]);
+        //         i++;
+        //         j++;
+        //     }
+        //     else if(nums1[i]<nums2[j])
+        //     {
+        //         i++;
+        //     }
+        //     else
+        //     {
+        //         j++;
+        //     }
             
-        }
+        // }
 
-        int []res=new int [set.size()];
-        int k=0;
-        for(int x:set)
-        {
-            res[k]=x;
-            k++;
-        }
-        return res;
+        // int []res=new int [set.size()];
+        // int k=0;
+        // for(int x:set)
+        // {
+        //     res[k]=x;
+        //     k++;
+        // }
+        // return res;
     }
 }
